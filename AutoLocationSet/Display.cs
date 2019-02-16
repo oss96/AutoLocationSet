1cbe0c7c9f5ecbd1cdb5e536d7ec5be4e83d0e56
oss96 <ossman96@gmail.com>
2019-02-16 14:57:41 +0100

    creation of the Program

---
+++ AutoLocationSet/Display.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLocationSet
+{
+    class Display
+    {
+        private string name;
+        private int height;
+        private int width;
+        private bool primary;
+
+        public Display()
+        {
+            this.name = "Display 1";
+            this.height = 2560;
+            this.width = 1440;
+            this.primary = true;
+        }
+        public Display(string inputName, int inputWidth, int inputHeight, bool inputPrimary)
+        {
+            this.name = inputName;
+            this.height = inputHeight;
+            this.width = inputWidth;
+            this.primary = inputPrimary;
+        }
+
+        internal bool IsPrimary()
+        {
+            return this.primary;
+        }
+        internal string GetName()
+        {
+            return this.name;
+        }
+        internal string GetResolution()
+        {
+            return this.width + " x " + this.height;
+        }
+        internal int GetWidth()
+        {
+            return this.width;
+        }
+        internal int GetHeight()
+        {
+            return this.height;
+        }
+    }
+}
---
+++ AutoLocationSet/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoLocationSet
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Display> displays = new List<Display>();
+            int i = 1;
+            foreach (var screen in Screen.AllScreens)
+            {
+                displays.Add(new Display(screen.DeviceName, screen.Bounds.Width, screen.Bounds.Height, screen.Primary));
+                i++;
+            }
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    listBox1.Items.Add("Process name: " + process.ProcessName);
+                    listBox1.Items.Add("ID: " + process.Id);
+                    listBox1.Items.Add("Window title: " + process.MainWindowTitle);
+                    listBox1.Items.Add("---------------------------------------");
+                }
+            }
+
+        }
+    }
+}
